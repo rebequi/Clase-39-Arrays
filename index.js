45204dc015fcb39d2baa52be8a39ae4d64751330
rebequi <rebk1998@icloud.com>
2020-08-24 18:48:47 -0600

    male aqui te mando los archivos de la Clase 18, no me funciono el ejercicio de los colores

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const primerElementoClaseVerde = document.querySelector (".verde")
+
+primerElementoClaseVerde.classList.remove("verde")
+
+const primerElementoClaseRojo = document.querySelector (".rojo")
+
+primerElementoClaseRojo.classList.add("rosa")
+
+const primerElementoClaseAmarillo = document.querySelector (".rosa")
+
+primerElementoClaseAmarillo.classList.toggle("rosa")
+
+const primerElementoClaseAmarillo = document.querySelector (".amarillo")
+
+primerElementoClaseAmarillo.classList.contains("rojo")
